2d57a8b0bd315a13485c650eb9164f7ed46e5b7e
Sara Carbaugh <sara@saracarbaugh.com>
2018-07-23 19:47:56 -0400

    added hackathon info

---
+++ week-2/wk2-hackathon.py
@@ -0,0 +1,48 @@
+# Build the 4x4 grid
+# randomly assign letters to the array
+# pull in dictionary
+# start at 0,0
+# loop through a walk method
+## is it a word?
+## if yes, keep going
+## if no, start over
+# sort dictionary options
+
+# we to:
+## Check the dictionary for a word
+## Building the string - add each character at a time
+## loop to the begining and run until it's NOT a word
+
+
+
+# create function to check if it's a word
+words = set()
+
+
+def boggle():
+	filename = "dictionary.txt"
+	dict = codecs.open(filename, "r", encoding='utf-8', errors='replace')
+	if word == some word in dict:
+		
+		return word
+		words.add()
+	
+a
+is "a" a word
+	
+	while it's not or if it's the beginning of a word,
+		keep walking
+	if it is True,
+		add it to the set
+	else you keep going
+		
+
+import codecs
+import string
+import pprint
+from collections import Counter
+from string import ascii_lowercase
+pp = pprint.PrettyPrinter(indent=4)
+
+filename = "dictionary.txt"
+dict = codecs.open(filename, "r", encoding='utf-8', errors='replace')
