3d185f7a7a43ba66b6773402b255cb02d1434bd2
Sara Carbaugh <sara@saracarbaugh.com>
2018-07-28 13:27:50 -0400

    added week 3 hackathon info

---
+++ week-3/wk3-hackathon.py
@@ -0,0 +1,27 @@
+# Keyword strategy for 1 Million Women To Tech
+# Step 1 Please read about Content Strategy here
+#
+# https://www.canva.com/learn/content-marketing-strategy/
+# https://blog.bufferapp.com/blog-growth-tactics
+# https://smartblogger.com/traffic-case-study/
+# Step 2
+#
+# Create a plain English writeup of your proposed Content Strategy - please write in Github Flavoured Markdown (like this document)
+# Include three main areas
+# Technology
+# Women and Enbies
+# The combination of the above two e.g. women in tech, enbies in tech
+# Optional: Python scrapy to crawl the web and create your own dictionary of keywords
+# https://scrapy.org/ Scrapy tutorials:
+# https://doc.scrapy.org/en/latest/intro/tutorial.html
+# https://www.digitalocean.com/community/tutorials/how-to-crawl-a-web-page-with-scrapy-and-python-3
+# https://www.analyticsvidhya.com/blog/2017/07/web-scraping-in-python-using-scrapy/
+# Step 3
+#
+# Create an implementation timeline that you suggest for the content
+# A channel by channel breakdown of the landscape
+# Make your suggestion for which keywords - that is, which TOPICs are of greatest interest and have the greatest need in the world
+# Include estimated traffic with each keyword/topic
+# BONUS
+#
+# Country specific ideas would be huge BONUS
